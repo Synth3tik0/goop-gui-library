e6a42483b0b863674b919e2f21d960bb8ebae34e
kiporshnikov <kiporshnikov@4a631193-2d15-b5e0-78eb-106c76b1a797>
2011-07-18 22:21:33 +0000

    Goop\UI\Checkbox.h Goop\UI\Checkbox.cpp [ADD] Added Checkbox control
    Goop\UI\Base.cpp
    [DEL] Removed old unused WM_COMMAND handler printing irrelevant debug info.
    [ADD] Added BM_SETCHECK handler for calling OnChecked and OnUnchecked functions on Checkboxes and Radio buttons.
    
    Goop\UI\Radio.cpp
    [ADD] Added Radio::OnChecked and Radio::OnUnchecked events.
    [FIX] Made Radio::GetChecked return the actual state of the button instead of just false.
    
    Goop\UI\Textbox.cpp
    [MOD] Edited the format of the CreateWindowEx call in the Textbox constructor so it looks a little nicer.

--- trunk/Goop/UI/Checkbox.h
+++ trunk/Goop/UI/Checkbox.h
@@ -1,16 +1,16 @@
-#ifndef RADIO_H
-#define RADIO_H
+#ifndef CHECKBOX_H
+#define CHECKBOX_H
 
 #include "Base.h"
 namespace Goop
 {
 	class MessageProxy;
 
-	class Radio : public Base
+	class Checkbox : public Base
 	{
 	public:
-		GOOP_API Radio(const wchar_t *text = L"", Base *parent = 0);
-		GOOP_API ~Radio();
+		GOOP_API Checkbox(const wchar_t *text = L"", Base *parent = 0);
+		GOOP_API ~Checkbox();
 
 		GOOP_API bool GetChecked();
 		GOOP_API void SetChecked(bool checked);
