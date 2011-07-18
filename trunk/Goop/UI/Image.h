88013ae69fdff60cb6e268efc36bcd54e8b8abf7
kiporshnikov <kiporshnikov@4a631193-2d15-b5e0-78eb-106c76b1a797>
2011-07-18 20:23:20 +0000

    Goop\UI\Image.cpp [MOD] Renamed Image::SetImage and Image::GetImage to Image::SetBitmap and Image::GetBitmap respectively to better explain their purpose.

--- trunk/Goop/UI/Image.h
+++ trunk/Goop/UI/Image.h
@@ -12,8 +12,8 @@ namespace Goop
 		GOOP_API Image(Bitmap *bitmap, Base *parent = 0);
 		GOOP_API ~Image();
 
-		GOOP_API virtual void SetImage(Bitmap *bitmap);
-		GOOP_API virtual Bitmap *GetImage();
+		GOOP_API virtual void SetBitmap(Bitmap *bitmap);
+		GOOP_API virtual Bitmap *GetBitmap();
 
 		GOOP_API virtual bool OnPaint();
 	private:
