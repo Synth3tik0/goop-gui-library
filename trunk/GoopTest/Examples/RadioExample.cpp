e6eab22c647711d44a76569ab4ebf6419aab554a
kiporshnikov <kiporshnikov@4a631193-2d15-b5e0-78eb-106c76b1a797>
2011-07-19 17:29:55 +0000

    GoopTest\Examples\RadioExample.cpp [MOD] RadioExample now also includes checkboxes.
    Goop\UI\Tab.h
    Goop\UI\Tab.cpp
    [MOD] Tabs now automatically fit to their TabContainer parent when parent is resized.

--- trunk/GoopTest/Examples/RadioExample.cpp
+++ trunk/GoopTest/Examples/RadioExample.cpp
@@ -4,7 +4,8 @@ using namespace Goop;
 const wchar_t *groupNames[] = 
 {
 	TEXT("Radio Group #1"),
-	TEXT("Radio Group #2")
+	TEXT("Radio Group #2"),
+	TEXT("Checkbox Group")
 };
 
 const wchar_t *radioNames[] = 
@@ -14,10 +15,22 @@ const wchar_t *radioNames[] =
 	TEXT("Radio Button Number 3"),
 	TEXT("Radio Button Number 4"),
 	TEXT("Radio Button Number 5"),
-	TEXT("Radio Button Number 6"),
-	TEXT("Radio Button Number 7"),
-	TEXT("Radio Button Number 8")
 };
+
+const wchar_t *checkNames[] = 
+{
+	TEXT("Checkbox Button Number 1"),
+	TEXT("Checkbox Button Number 2"),
+	TEXT("Checkbox Button Number 3"),
+	TEXT("Checkbox Button Number 4"),
+	TEXT("Checkbox Button Number 5"),
+	TEXT("Checkbox Button Number 6"),
+	TEXT("Checkbox Button Number 7"),
+	TEXT("Checkbox Button Number 8"),
+	TEXT("Checkbox Button Number 9"),
+	TEXT("Checkbox Button Number 10"),
+};
+
 
 class ExampleRadio : public Radio
 {
@@ -35,6 +48,28 @@ public:
 	}
 };
 
+class ExampleCheckbox : public Checkbox
+{
+public:
+	ExampleCheckbox(const wchar_t *text, Base *parent) : Checkbox(text, parent) {}
+
+	bool OnChecked()
+	{
+		g_outputBox->AppendText(GetText());
+		g_outputBox->AppendText(TEXT(" was checked!\r\n"));
+
+		return false;
+	}
+
+	bool OnUnchecked()
+	{
+		g_outputBox->AppendText(GetText());
+		g_outputBox->AppendText(TEXT(" was UNchecked!\r\n"));
+
+		return false;
+	}
+};
+
 void RadioExample(TabContainer *tabContainer)
 {
 	g_outputBox->AppendText(TEXT("Created Label Example tab\r\n"));
@@ -45,14 +80,29 @@ void RadioExample(TabContainer *tabContainer)
 	{
 		Group *group = new Group(groupNames[g], tab);
 		group->SetPosition(Vector2D(10 + (g * 282), 10));
-		group->SetSize(Vector2D(272, 200));
+		group->SetSize(Vector2D(272, 150));
 
-		for(int r = 0; r < 8; r++)
+		for(int r = 0; r < 5; r++)
 		{
 			ExampleRadio *radio = new ExampleRadio(radioNames[r], group);
 			radio->SetBackgroundColor(255, 255, 255);
 			radio->SetSize(Vector2D(250, 20));
-			radio->SetPosition(Vector2D(10, 20 + (r * 22)));
+			radio->SetPosition(Vector2D(10, 20 + (r * 25)));
+		}
+	}
+
+	Group *group = new Group(groupNames[2], tab);
+	group->SetPosition(Vector2D(10, 165));
+	group->SetSize(Vector2D(554, 160));
+
+	for(int i = 0; i < 2; i++)
+	{
+		for(int c = 0; c < 5; c++)
+		{
+			ExampleCheckbox *check = new ExampleCheckbox(checkNames[i * 5 + c], group);
+			check->SetBackgroundColor(255, 255, 255);
+			check->SetSize(Vector2D(250, 20));
+			check->SetPosition(Vector2D(10 + (282 * i), 20 + (c * 27)));
 		}
 	}
 }
