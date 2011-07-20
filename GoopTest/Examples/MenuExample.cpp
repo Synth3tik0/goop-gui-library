c73c1201ead0a7e1011626231ca95d4d799b8f5f
kiporshnikov <kiporshnikov@4a631193-2d15-b5e0-78eb-106c76b1a797>
2011-07-20 10:29:25 +0000

    GoopTest\Examples\MenuExample.cpp [FIX] Menu example now properly reports what it's doing.

--- GoopTest/Examples/MenuExample.cpp
+++ GoopTest/Examples/MenuExample.cpp
@@ -8,13 +8,24 @@ public:
 
 	bool OnMouseClick(MouseButton button)
 	{
-		SetChecked(!GetChecked());
+		if(GetChecked())
+		{
+			SetChecked(false);
+			g_outputBox->AppendText(TEXT("Unchecked menu item\r\n"));
+		}
+		else
+		{
+			SetChecked(true);
+			g_outputBox->AppendText(TEXT("Checked menu item\r\n"));
+		}
 		return false;
 	}
 };
 
 void MenuExample(Window *window)
 {
+	g_outputBox->AppendText(TEXT("Created Menu Example\r\n"));
+
 	Menu *menu = new Menu();
 	Menu *first = new Menu(menu, TEXT("Hello world!"));
 
