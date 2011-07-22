19020f9d208865dea9cae4517e13b620a3cf70b7
kiporshnikov <kiporshnikov@4a631193-2d15-b5e0-78eb-106c76b1a797>
2011-07-22 21:09:20 +0000

    Merged Branch.

--- trunk/Goop/UI/TreeNode.cpp
+++ trunk/Goop/UI/TreeNode.cpp
@@ -4,7 +4,7 @@ using namespace Goop;
 
 TreeNode::TreeNode()
 {
-
+	m_info.mask = TVIF_CHILDREN | TVIF_HANDLE | TVIF_IMAGE | TVIF_PARAM | TVIF_SELECTEDIMAGE | TVIF_STATE | TVIF_TEXT;
 }
 
 TreeNode::~TreeNode()
